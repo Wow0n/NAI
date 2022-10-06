563956da5b43e2b15c7bc45caf1f54e7918ac172
Wow0n <mati.maxx@o2.pl>
2022-10-06 10:17:19 +0200

    lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,24 +9,25 @@
 using namespace std;
 
 void lab1(function<double(vector<double>)> operation, vector<double> numbers) {
-    cout << operation(numbers);
+    cout << operation(numbers) << endl;
 }
 
 int main(int argc, char **argv) {
     vector<double> argNumbers;
-    map<string, function<double(vector<double>)>> operations;
+    map<string, map<string, function<double(vector<double>)>>> operations;
 
-    operations["sin"] = [](vector<double> numbers) { return sin(numbers[0]); };
-    operations["mod"] = [](vector<double> numbers) { return fmod(numbers[0], numbers[1]); };
-    operations["add"] = [](vector<double> numbers) { return numbers[0] + numbers[1]; };
+    operations["lab1"]["sin"] = [](vector<double> numbers) { return sin(numbers[0]); };
+    operations["lab1"]["mod"] = [](vector<double> numbers) { return fmod(numbers[0], numbers[1]); };
+    operations["lab1"]["add"] = [](vector<double> numbers) { return numbers[0] + numbers[1]; };
 
     try {
+        char* functionName = argv[1];
         char* operationName = argv[2];
         for (int i = 3; i < argc; i++) {
             argNumbers.push_back(stod(argv[i]));
         }
 
-        lab1(operations.at(operationName), argNumbers);
+        lab1(operations.at(functionName).at(operationName), argNumbers);
     } catch (std::out_of_range aor) {
         return 1;
     }
