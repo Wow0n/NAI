8da940db2d858c68884f1a8f746e185f20fc3439
Mateusz Łuczak <mati.maxx@o2.pl>
2022-10-12 16:57:39 +0200

    lab1 fix

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -14,20 +14,20 @@ void lab1(function<double(vector<double>)> operation, vector<double> numbers) {
 
 int main(int argc, char **argv) {
     vector<double> argNumbers;
-    map<string, map<string, function<double(vector<double>)>>> operations;
+    map<string, function<double(vector<double>)>> operations;
 
-    operations["lab1"]["sin"] = [](vector<double> numbers) { return sin(numbers[0]); };
-    operations["lab1"]["mod"] = [](vector<double> numbers) { return fmod(numbers[0], numbers[1]); };
-    operations["lab1"]["add"] = [](vector<double> numbers) { return numbers[0] + numbers[1]; };
+    operations["sin"] = [](vector<double> numbers) { return sin(numbers[0]); };
+    operations["mod"] = [](vector<double> numbers) { return fmod(numbers[0], numbers[1]); };
+    operations["add"] = [](vector<double> numbers) { return numbers[0] + numbers[1]; };
 
     try {
-        char* functionName = argv[1];
-        char* operationName = argv[2];
-        for (int i = 3; i < argc; i++) {
+        char* operationName = argv[1];
+
+        for (int i = 2; i < argc; i++) {
             argNumbers.push_back(stod(argv[i]));
         }
 
-        lab1(operations.at(functionName).at(operationName), argNumbers);
+        lab1(operations.at(operationName), argNumbers);
     } catch (std::out_of_range aor) {
         return 1;
     } catch (std::logic_error e) {
